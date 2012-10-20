a28c7dcd1caa3c84d6c617df151338d426b8fe47
huang47 <huge.huang@gmail.com>
2012-10-20 20:17:03 +0800

    updated event hover

--- rexchrome/text-select.js
+++ rexchrome/text-select.js
@@ -1,6 +1,6 @@
 YUI({
     gallery: 'gallery-2012.10.03-20-02'
-}).use('io', 'gallery-event-selection', function (Y, NAME) {
+}).use('io', 'gallery-event-selection', 'event-hover', function (Y, NAME) {
     // Fired only on text selection
     var sub = Y.Lang.sub,
         d = Y.config.doc,
@@ -8,6 +8,11 @@ YUI({
         body = Y.one('body'),
         API = 'http://yhack.piliapp.com/editor/?title={title}&url={url}&text={text}&pos={pos}';
 
+    /**
+     * @private
+     * @method getTitle
+     * @description return h1 or h2 or selected text
+     **/
     function getTitle() {
         var h1 = Y.one('h1'),
             h2 = Y.one('h2'),
@@ -24,6 +29,29 @@ YUI({
         return title;
     }
 
+    /**
+     * @private
+     * @method attachImage
+     * @param {Object} pos position object which contains pageX and pageY
+     * @param {String} url image url
+     **/
+    function attachImage(pos, url) {
+        var img = new Image(),
+            elem,
+            parent;
+
+        img.src = url;
+        elem = elementFromPoint(pos.x, pos.y);
+        parent = elem.get('parent');
+
+        if (parent) {
+            parent._node.insertBefore(img, elem._node);
+        }
+    }
+
+    /**
+     * @event selection
+     **/
     body.on('selection', function(e) {
         var data = {
             title: getTitle(),
@@ -32,10 +60,18 @@ YUI({
             xy: [e.pageX, e.pageY].join(',')
         };
 
+        Y.log(data, 'data');
+
         Y.on('io:complete', function (id, o) {
             console.log(o, 'result');
         });
 
-        Y.io(sub(API, data));
+        // should go to canvas editing page
+        window.open(sub(API, data));
     });
+
+    Y.on('hover', function (e) {
+        var el = e.currentTarget;
+        console.log(el.get('src'), 'src');
+    }, function (e) {}, 'img');
 });
