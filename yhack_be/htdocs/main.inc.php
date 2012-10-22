e3f7b95f4822de23b12ebaaa4013ad7a16b814d4
yhacker <ubuntu@ip-10-120-16-91.ap-northeast-1.compute.internal>
2012-10-22 02:50:19 +0000

    change fb app secret and make secret key invisible

--- yhack_be/htdocs/main.inc.php
+++ yhack_be/htdocs/main.inc.php
@@ -2,5 +2,9 @@
 
 define('ROOT_PATH', '/var/www/yhack_be');
 define('FB_APP_ID', '479655635388342');
-define('FB_APP_SECRET' , 'ffa8f51e3280805d1a7dbc9d5c5f8366');
 define('FB_APP_SCOPE', 'user_likes,publish_stream,read_stream,photo_upload');
+
+// TODO: you have to setup secret.inc.php manually
+// define('FB_APP_SECRET' , 'FB app secret key');
+require_once ROOT_PATH . '/secret.inc.php';
+
